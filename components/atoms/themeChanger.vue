c9fbf9dc9912e1fa2b3aaef03ac53b1321110319
alalan55 <alla.ao4975@gmail.com>
2023-11-19 09:54:05 -0300

    Insere ajuste em mixins de cores

--- components/atoms/themeChanger.vue
+++ components/atoms/themeChanger.vue
@@ -1,57 +1,66 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
-const currentTheme = ref<Number>(0);
+interface Theme {
+  path: string;
+  alt: string;
+}
+
 const colorMode = useColorMode();
 colorMode.preference = "light";
 
-const lightTheme = ref<Object>({
+const lightTheme = ref<Theme>({
   path: "/icons/sun-icon.svg",
   alt: "Light",
 });
 
-const darkTheme = ref<Object>({
+const darkTheme = ref<Theme>({
   path: "/icons/moon-icon.svg",
   alt: "Dark",
 });
 
-const toggleTheme = () => {
-  // currentTheme.value == 0 ? (currentTheme.value = 1) : (currentTheme.value = 0);
+const currentTheme = computed(() => {
+  let theme = colorMode.preference;
+  return theme;
+});
 
-  if (currentTheme.value == 0) {
-    currentTheme.value = 1;
-    colorMode.preference = "dark";
-  } else {
-    currentTheme.value = 0;
-    colorMode.preference = "light";
-  }
-};
+const toggleTheme = () =>
+  currentTheme.value == "light"
+    ? (colorMode.preference = "dark")
+    : (colorMode.preference = "light");
 </script>
 
 <template>
   <figure @click="toggleTheme">
     <img
-      :src="currentTheme ? darkTheme.path : lightTheme.path"
-      :alt="currentTheme ? darkTheme.alt : lightTheme.alt"
+      :src="currentTheme == 'dark' ? darkTheme.path : lightTheme.path"
+      :alt="currentTheme == 'dark' ? darkTheme.alt : lightTheme.alt"
     />
   </figure>
 </template>
 
 <style lang="scss">
 @import "@/assets/main.scss";
-figure {
-  width: 35px;
-  height: 35px;
-  cursor: pointer;
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
+
+body {
+  transition: 0.2s ease-in-out;
+
+  figure {
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
   }
 }
-
-// criar no mixins minhas escolhas de cores para fazer o correto funcionamento do dark/light mode
 .dark-mode body {
   @include darkTheme;
+}
+
+.light-mode body {
+  @include lightTheme;
 }
 </style>
